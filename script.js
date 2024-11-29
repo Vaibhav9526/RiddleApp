a7c1432902d2619c8b86b9090afee37311a02d85
vaibhav9526 <vaibhavsharma9526@gmail.com>
2024-11-30 01:26:54 +0530

    updated ui , added json , preloader added , improve js

--- script.js
+++ script.js
@@ -1,41 +1,54 @@
-const apiurl = "https://riddles-api.vercel.app/random";
-
-const answer = document.querySelector('.showAns');
-const nextbtn = document.querySelector('.nextmeme');
+const preloader = document.querySelector('.brain-loader');
+const Page = document.querySelector('.Main');
 const setup = document.querySelector('.setup');
+const answer = document.querySelector('.showAns');
 const delivery = document.querySelector('.delivery');
+const nextbtn = document.querySelector('.nextmeme');
+const apiurl = "./data.json";
+
+window.addEventListener('load' , ()=>{
+  setTimeout(()=>{
+    preloader.style.display = 'none';
+    Page.style.display = 'block';
+   } , 3000)
+});
+
+let usedIndices = new Set();
 
 async function riddles() {
   const response = await fetch(apiurl);
-  
   var data = await response.json();
-  setup.innerHTML = data.riddle;
+ 
+  if (usedIndices.size === data.length) {
+    console.error("No more unique riddles left!");
+    return; 
+  }
 
+  let randomIndex;
+  do {
+    randomIndex = Math.floor(Math.random() * data.length); 
+  } while (usedIndices.has(randomIndex));
+
+  usedIndices.add(randomIndex); 
+  setup.innerHTML = data[randomIndex].riddle;
   answer.style.display = "block";
-  delivery.style.display="none";
-  console.log(data);
-  console.log("vaibhav");
+  delivery.style.display = "none";
 
-  answer.addEventListener("click", function() {
+answer.addEventListener("click", function() {
     answer.style.display = "none";
     delivery.style.display = "block";
-    delivery.innerHTML = data.answer;
-});
-// delivery.style.display = "none";
-  
-
+    delivery.innerHTML = data[randomIndex].answer;
+  });
 }
 
 answer.addEventListener("click", () => {
-  delivery.style.display='block';
+  delivery.style.display = 'block';
 });
 
 nextbtn.addEventListener("click", () => {
-  riddles();
+  riddles(); 
 });
 
 window.addEventListener("load", () => {
-    riddles();
+  riddles(); 
 });
-
-// @Again leaving for 7 months bye! ( today 6/11/24)
